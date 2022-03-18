015267ee46838962f1e2c80f9c53694087313034
Justin Quinn <jpq3366@gmail.com>
2022-03-18 12:49:45 -0400

    Added solutions from 03-18-22

--- Codewars/8kyu/03-18-22.js
+++ Codewars/8kyu/03-18-22.js
@@ -10,12 +10,3 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
       return false;
     }
   }
-
-// Sample Tests
-const assert = require("chai").assert;
-
-describe("zeroFill", function() {
-  it("Sample Tests", function() {
-    assert.equal(zeroFuel(50, 25, 2), true);
-    assert.equal(zeroFuel(100, 50, 1), false);
-  });
