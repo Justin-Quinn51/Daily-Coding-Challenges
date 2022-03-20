950ceca20c761a78f3eb618f792f2244b8fbaf60
Justin Quinn <jpq3366@gmail.com>
2022-03-20 12:56:41 -0400

    Added solutions from 03-20-22

--- Codewars/8kyu/03-20-22.js
+++ Codewars/8kyu/03-20-22.js
@@ -2,33 +2,35 @@
 
 /*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
 
-// Solution
-// function even_or_odd(number) {
-//   if (number % 2 === 0) {
-//     return 'Even'
-//   } else {
-//     return "Odd"
-//   }
-// }
+Solution
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    return 'Even'
+  } else {
+    return "Odd"
+  }
+}
 
 // Convert a Number to a String!
 
-// We need a function that can transform a number into a string.
+/*We need a function that can transform a number into a string*/
 
 // Solution 
-// function numberToString(num) {
-//     return String(num);
-//   }
+
+ function numberToString(num) {
+    return String(num);
+  }
 
 // Convert boolean values to strings 'Yes' or 'No'
 
 /*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
 
 // Solution
-// function boolToWord( bool ){
-//     if (bool === true) {
-//       return 'Yes'
-//     } else {
-//       return 'No'
-//     }
-//   }
+
+function boolToWord( bool ){
+    if (bool === true) {
+      return 'Yes'
+    } else {
+      return 'No'
+    }
+  }
