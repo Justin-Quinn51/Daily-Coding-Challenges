33f8dccceb43d6083ae68eaea1a58d3cec0678aa
Justin Quinn <jpq3366@gmail.com>
2022-03-23 13:06:50 -0400

    Added solutions from 03-23-22

--- Codewars/8kyu/03-23-22.js
+++ Codewars/8kyu/03-23-22.js
@@ -3,11 +3,15 @@
 /*Complete the square sum function so that it squares each number passed into it and then sums the results together.*/
 
 // Solution 
+
 function squareSum(numbers) {
     return numbers.reduce((sum, num) => sum + (num * num), 0)
   }
 
 // Remove String Spaces
 
+/*Simple, remove the spaces from the string, then return the resultant string.*/
+
 // Solution 
+
 function noSpace(x){return x.split(' ').join('')}
