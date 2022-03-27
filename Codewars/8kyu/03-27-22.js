49c042f842cdced39f9d885a111310859c3f2f3e
Justin Quinn <jpq3366@gmail.com>
2022-03-27 13:17:42 -0400

    Added solutions from 03-27-22

--- Codewars/8kyu/03-27-22.js
+++ Codewars/8kyu/03-27-22.js
@@ -1,9 +1,11 @@
 // Get the mean of an array
 
 // Solution
+
 function getAverage(marks){
     return Math.floor(marks.reduce((acc, c) => acc + c) / marks.length)
   }
 
 // Arrow function refactor
+
 const getAverage = marks => Math.floor(marks.reduce((acc, c) => acc + c) / marks.length)
