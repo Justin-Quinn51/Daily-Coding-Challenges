888bd90a4e646ff42a17c1a4d90f38a6b93fc6bb
Justin Quinn <jpq3366@gmail.com>
2022-03-28 17:20:48 -0400

    Fixed formatting issues

--- Codewars/8kyu/03-28-22.js
+++ Codewars/8kyu/03-28-22.js
@@ -1,6 +1,7 @@
 // Fake Binary
 
-/*Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.*/
+/*Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. 
+Return the resulting string.*/
 
 // Solution
 
