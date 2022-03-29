734621adbf7639f664ccff582784243580e5422f
Justin Quinn <jpq3366@gmail.com>
2022-03-29 13:20:27 -0400

    Added solutions from 03-29-22

--- Codewars/8kyu/03-29-22.js
+++ Codewars/8kyu/03-29-22.js
@@ -1,12 +1,10 @@
 // You only need one - Beginner
 
-/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
-
-Array can contain numbers or strings. X can be either.
-
+/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value. Array can contain numbers or strings. X can be either.
 Return true if the array contains the value, false.*/
 
 // Solution
+
 function check(a, x) {
     return a.includes(x) ? true : false; 
    }
