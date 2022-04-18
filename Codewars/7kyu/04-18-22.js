7fe2da03a8be014286253b6fdbf85fc66b4f3345
Justin Quinn <jpq3366@gmail.com>
2022-04-18 13:27:05 -0400

    Added solutions from 04-18-22

---
+++ Codewars/7kyu/04-18-22.js
@@ -0,0 +1,17 @@
+// Odd or Even
+
+/*Given a list of integers, determine whether the sum of its elements is odd or even.
+Give your answer as a string matching "odd" or "even".
+If the input array is empty consider it as: [0] (array with a zero).*/
+
+// Solution
+
+// function oddOrEven(array) {
+//  if(array.reduce( (acc, c) => acc + c, 0) % 2 === 0) {
+//    return 'even'
+//  } else {
+//    return 'odd' 
+//  }
+// }
+
+const oddOrEven = array => array.reduce( (acc, c) => acc + c, 0) % 2 === 0 ? 'even' : 'odd'
