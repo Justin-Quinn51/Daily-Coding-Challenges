288f08590841a958b3747e1f2a31ebc0063324fe
Justin Quinn <jpq3366@gmail.com>
2022-04-18 13:23:29 -0400

    Added solutions from 04-18-22

---
+++ Codewars/8kyu/04-18-22.js
@@ -0,0 +1,32 @@
+// Leonardo Dicaprio and Oscars
+
+/*f oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+if it was over 88 you should return "Leo got one already!"*/
+
+// Solution
+
+function leo(oscar){
+    if (oscar > 88) {
+      return 'Leo got one already!'
+    } else if (oscar === 88) {
+      return 'Leo finally won the oscar! Leo is happy'
+    } else if (oscar !== 86 && oscar !== 88) {
+      return 'When will you give Leo an Oscar?'
+    } else if (oscar === 86) {
+      return 'Not even for Wolf of wallstreet?!'
+    }
+  }
+
+// Get Nth Even Number
+
+/*Return the Nth Even Number. The input will not be 0.*/
+
+// Solution
+
+function nthEven(n){
+    return (n - 1) * 2
+  }
+
+
