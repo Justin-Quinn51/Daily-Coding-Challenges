c5340c603817f13dec8b61347ef8535ae423cf86
Justin Quinn <jpq3366@gmail.com>
2022-04-21 00:24:48 -0400

    Added solutions from 04-21-22

---
+++ Codewars/8kyu/04-21-22.js
@@ -0,0 +1,19 @@
+// Returning Strings
+
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".*/
+
+// Solution
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+// Convert a Boolean to a String
+
+/*mplement a function which convert the given boolean value into its string representation.*/
+
+// Solution
+
+function booleanToString(b){
+  return b.toString()
+}
