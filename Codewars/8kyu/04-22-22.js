32fd2c0197270e3c98013e0c6d8c19fc3b187f8b
Justin Quinn <jpq3366@gmail.com>
2022-04-22 12:25:01 -0400

    Added solutions from 04-22-22

---
+++ Codewars/8kyu/04-22-22.js
@@ -0,0 +1,19 @@
+// You Can't Code Under Pressure #1
+
+/*Code as fast as you can! You need to double the integer and return it.*/
+
+// Solution
+
+function doubleInteger(i) {
+    return i * 2
+  }
+
+// Invert Values
+
+/*Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.*/
+
+// Solution
+
+function invert(array) {
+    return array.map(num => -num)
+   }
