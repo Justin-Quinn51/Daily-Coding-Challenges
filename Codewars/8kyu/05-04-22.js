844fdbb12ffd4d0ad7cb9335234869aac2344160
Justin Quinn <jpq3366@gmail.com>
2022-05-04 23:31:11 -0400

    Added solutions from 05-04-22

---
+++ Codewars/8kyu/05-04-22.js
@@ -0,0 +1,24 @@
+// Is it even?
+
+/*In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+ */
+
+// Solution
+
+function testEven(n) {
+    return n % 2 === 0
+  }
+
+// Grasshopper - Debug sayHello
+
+/*The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!*/
+
+// Solution
+
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
