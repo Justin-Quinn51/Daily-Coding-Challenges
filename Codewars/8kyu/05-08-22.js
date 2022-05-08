4ae809294370262d15e78e9ca5783852ae2fed36
Justin Quinn <jpq3366@gmail.com>
2022-05-08 11:38:07 -0400

    Added solutions from 05-08-22

---
+++ Codewars/8kyu/05-08-22.js
@@ -0,0 +1,21 @@
+// Grasshopper - Function syntax debugging
+
+/*A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.*/
+
+// Solution
+
+function main (verb, noun) {
+    return verb + noun
+  }
+
+// Grasshopper - Basic Function Fixer
+
+/* I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+Can you help me fix the function?*/
+
+// Solution
+
+function addFive(num) {
+    let total = num + 5
+    return total
+  }
