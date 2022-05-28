b9e4b218a67a17bcace0453b3914d7665f361c58
Justin Quinn <jpq3366@gmail.com>
2022-05-27 23:25:23 -0400

    Added solution from 05-27-22

---
+++ Codewars/8kyu/05-27-22.js
@@ -0,0 +1,9 @@
+// repeatIt
+
+/*Create a function that takes a string and an integer (n).
+The function should return a string that repeats the input string n number of times.
+If anything other than a string is passed in you should return "Not a string"*/
+
+// Solution
+
+const repeatIt = (str,n) => typeof str == 'string' ? str.repeat(n) : 'Not a string'
