2bcecbcf86f9438d1d8f0a2892bd37e758bd8fef
Justin Quinn <jpq3366@gmail.com>
2022-06-04 01:32:14 -0400

    Added solutions from 06-04-22

--- Codewars/7kyu/06-04-22.js
+++ Codewars/7kyu/06-04-22.js
@@ -1,6 +1,6 @@
 // Narcissistic Numbers 
 
-/* A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. Write a function that, given n, returns whether or not n is a Narcissistic Number.*/
+/* A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. Write a function that, given n, returns whether or not n is a Narcissistic Number. */
 
 // Solution
 
