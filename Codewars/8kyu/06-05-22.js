e3e93c49f8423dc0d24b87fa2069193007bcb102
Justin Quinn <jpq3366@gmail.com>
2022-06-05 09:00:20 -0400

    Added solutions from 06-05-22

---
+++ Codewars/8kyu/06-05-22.js
@@ -0,0 +1,19 @@
+// Template Strings
+
+/*Your task is to return the correct string using the Template String Feature.*/
+
+// Solution
+
+const TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+  }
+
+// Are arrow functions odd?
+
+/*ime to test your basic knowledge in functions! Return the odds from a list:*/
+
+// Solution
+function odds(values){
+    // arrow it
+    return values.filter(value => value % 2 === 1 );
+  }
