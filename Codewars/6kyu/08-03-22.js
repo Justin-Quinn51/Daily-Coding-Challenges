c5e39298cdd4bf70d20977a1d159f425b058aaa2
Justin Quinn <jpq3366@gmail.com>
2022-08-03 14:51:31 -0400

    Added solution from 08-03-22

---
+++ Codewars/6kyu/08-03-22.js
@@ -0,0 +1,43 @@
+// Message Validator
+
+/*In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
+
+For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
+
+Notes:
+
+Messages are composed of only letters and digits
+Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
+Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
+If the message is an empty string, you should return true*/
+
+// Solution
+
+function isAValidMessage(message){
+let words = message.split(/[0-9]/g).filter(string => string !== '')
+let numbers = message.split(/[a-z]/gi).filter(string => string !== '').map(e => +e)
+
+  
+  if (message === "") {
+    return true
+  }
+  
+  if (isNaN(message[0])|| !isNaN(message.charAt(message.length - 1)) ) {
+    return false
+  }
+
+  if (words.length !== numbers.length) {
+    return false
+  }
+  
+  for (let index = 0; index < words.length; index++) {
+    if (words[index].length !== numbers[index]) {
+      return false
+    } 
+  }
+  
+ return true
+
+  
+  
+}
