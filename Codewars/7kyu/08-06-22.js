1e9f9ba82b29ba729a5805ac5140d6452f5a096d
Justin Quinn <jpq3366@gmail.com>
2022-08-06 20:13:48 -0400

    Added solution from 08-06-22

---
+++ Codewars/7kyu/08-06-22.js
@@ -0,0 +1,16 @@
+// Switcheroo
+
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+// Solution
+
+function switcheroo(x){
+ return x.split('').map( (e) => e === 'a' ? 'b' : e === 'b' ? 'a' : 'c').join('')
+}
