5ae349c8e073f3e186c16cb72ebdf61fc9b769d2
Justin Quinn <jpq3366@gmail.com>
2023-04-16 18:21:05 -0400

    Complete 'Remove anchor from URL' from Codewars

---
+++ Codewars/7kyu/04-16-23.js
@@ -0,0 +1,11 @@
+// Remove anchor from URL
+
+/*
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+*/
+
+// Solution
+
+function removeUrlAnchor(url) {
+  return url.split('#')[0];
+}
