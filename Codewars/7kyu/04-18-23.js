60bb6e8b13524df40feb868a216da0fe782c6889
Justin Quinn <jpq3366@gmail.com>
2023-04-18 16:09:07 -0400

    Compete 'Sort the gift code' from Codewars

---
+++ Codewars/7kyu/04-18-23.js
@@ -0,0 +1,11 @@
+// Sort the Gift Code
+
+/*
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+*/
+
+// Solution
+
+function sortGiftCode(code){
+  return code.split('').sort().join('');
+}
