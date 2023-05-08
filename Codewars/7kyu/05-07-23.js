d2dd32a621b8eef0cc439f0655981f58a9ad3d09
Justin Quinn <jpq3366@gmail.com>
2023-05-07 23:41:01 -0400

    Complete 'Remove duplicate words' from Codewars

---
+++ Codewars/7kyu/05-07-23.js
@@ -0,0 +1,23 @@
+// Remove duplicate words
+
+/*
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+*/
+
+// Solution
+
+function removeDuplicateWords (s) {
+  let arr = s.split(' ')
+  let noRepeats = [...new Set(arr)]
+  return noRepeats.join(' ')
+}
