79625f986f4cedcac05ef32ef732ade2a672894b
Justin Quinn <jpq3366@gmail.com>
2023-05-09 15:16:31 -0400

    Complete 'Filter the number' from Codewars

---
+++ Codewars/7kyu/05-09-23.js
@@ -0,0 +1,19 @@
+// Filter the number
+
+/*
+Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+Task
+Your task is to return a number from a string.
+
+Details
+You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+// Solution
+
+var filterString = function(value) {
+  let onlyDigits = value.replace(/\D/g, '')
+  let convertedNums = Number(onlyDigits)
+  return convertedNums
+}
