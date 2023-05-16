0ab47650ad2f7c2a345b7a4abeeacdbb6ad7fbf2
Justin Quinn <jpq3366@gmail.com>
2023-05-16 13:44:30 -0400

    Complete 'All Unique' from Codewars

---
+++ Codewars/7kyu/05-16-23.js
@@ -0,0 +1,13 @@
+// All Unique
+
+/*
+Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+*/
+
+// Solution
+function hasUniqueChars(str) {
+  let uniqueChars = new Set(str).size === str.length;
+  return uniqueChars;
+}
