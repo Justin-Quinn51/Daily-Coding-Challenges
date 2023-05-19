c601d77f55534fbcb64a038815120e3bf574849f
Justin Quinn <jpq3366@gmail.com>
2023-05-19 15:22:24 -0400

    Complete 'Incrementer' from Codewars

---
+++ Codewars/7kyu/05-19-23.js
@@ -0,0 +1,16 @@
+// Incrementer
+
+/*
+Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+
+Notes:
+return an empty array if your array is empty
+arrays will only contain numbers so don't worry about checking that
+*/
+
+// Solution
+function incrementer(nums) {
+  return nums.map((num, index) => (num + index + 1) % 10);
+}
